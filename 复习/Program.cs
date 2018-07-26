30b6011bda28c65f0d2d36a073e50c50f5178f3b
扬 童 <452949529@qq.com>
2018-07-26 10:47:50 +0800

    File类的一些补充知识

--- 复习/Program.cs
+++ 复习/Program.cs
@@ -24,13 +24,35 @@ namespace 复习
             }
             if (File.Exists(@"C:\Users\45294\Desktop\1.txt"))
             {
-                byte[] buffer = File.ReadAllBytes(Path.GetFullPath(@"C:\Users\45294\Desktop\1.txt"));
-                string s = Encoding.GetEncoding("UTF-8").GetString(buffer);
-                Console.WriteLine(s);
+                // 按byte流进行读取
+                //byte[] buffer = File.ReadAllBytes(Path.GetFullPath(@"C:\Users\45294\Desktop\1.txt"));
+                //string s = Encoding.GetEncoding("UTF-8").GetString(buffer);
+                //Console.WriteLine(s);
+
+                // 按行读取
+                //string[] strs = File.ReadAllLines(@"C:\Users\45294\Desktop\1.txt", Encoding.UTF8);
+                //Hashtable ht = new Hashtable();
+                //foreach (string item in strs)
+                //{
+                //    string[] contect = item.Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+                //    ht[contect[0]] = contect[1];
+                //}
+
+                //foreach (var item in ht.Keys)
+                //{
+                //    Console.WriteLine(ht[item]);
+                //}
+
+                // 按文本读取
+                //string str = File.ReadAllText(@"C:\Users\45294\Desktop\1.txt", Encoding.UTF8);
+                //Console.WriteLine(str);
             }
             else
             {
-                Console.WriteLine("指定路径不存在！");
+                string path = @"C:\Users\45294\Desktop\1.txt";
+                string content = "asdadas啥都看的看见爱上大框架爱好的空间啊";
+                File.WriteAllBytes(path, Encoding.UTF8.GetBytes(content));
+                Console.WriteLine("文件创建成功");
             }
             
 
