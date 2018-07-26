d6c5ab9d11b8a41cb6a35bfbd71447671db5b7f3
扬 童 <452949529@qq.com>
2018-07-26 14:08:21 +0800

    装箱与拆箱

---
+++ 装箱与拆箱/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 装箱与拆箱
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            //ArrayList list = new ArrayList();
+            List<int> list = new List<int>();
+            sw.Start();
+            for (int i = 0; i < 100000000; i++)
+            {
+                list.Add(i);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+            Console.ReadKey();
+
+            //00:00:12.1788167
+            //00:00:01.0935889
+
+            int n = 1;
+            IComparable j = n; // 装箱
+        }
+    }
+}
